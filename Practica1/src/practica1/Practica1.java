3a5c026e09b6cee7a5c4564196f64ac2aec39e4d
hugoarreaga <halexander210@gmail.com>
2019-12-07 00:08:55 -0600

    Interfaz Grafica

--- Practica1/src/practica1/Practica1.java
+++ Practica1/src/practica1/Practica1.java
@@ -15,7 +15,9 @@ public class Practica1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        // TODO code application logic here
+        Intefaz f = new Intefaz();
+        f.setVisible (true);
     }
     
 }
